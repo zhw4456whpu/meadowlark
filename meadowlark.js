43a57438019eb69c58ce4bb6720956ef013f74db
zhw4456whpu <zhw4456whpu@163.com>
2017-09-13 01:11:39 +0800

    时间：2017-09-13 文件：如下 描述：创建新项目meadowlark框架 提交人：winterlz 影响范围：整个项目

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var meadowlark = express();
 meadowlark.set('port', process.env.PORT || 3000);
+
+// 设置handlebars 视图引擎
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+meadowlark.engine('handlebars', handlebars.engine);
+meadowlark.set('view engine', 'handlebars');
+
+meadowlark.get('/', function(req, res){
+	res.render('home');
+});
+meadowlark.get('/about', function(req, res){
+	res.render('about');
+});
+
+
 // 定制404 页面
 meadowlark.use(function(req, res){
 	res.type('text/plain');
@@ -14,8 +28,8 @@ meadowlark.use(function(err, req, res, next){
 	res.status(500);
 	res.send('500 - Server Error');
 });
-meadowlark.listen(meadowlark.get('port'), function(){
+/*meadowlark.listen(meadowlark.get('port'), function(){
 	console.log( 'Express started on http://localhost:' + meadowlark.get('port') + '; press Ctrl-C to terminate.' );
-});
+});*/
 
 module.exports = meadowlark;
