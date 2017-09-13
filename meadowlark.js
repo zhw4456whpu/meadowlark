567d09df8b9ad945197f6f4ea77327f73e41b299
zhw4456whpu <zhw4456whpu@163.com>
2017-09-13 11:29:04 +0800

    时间：2017-09-13 文件：如下 描述：about页面随机发放饼干 提交人：winterlz 影响范围：整个项目

--- meadowlark.js
+++ meadowlark.js
@@ -11,29 +11,47 @@ var handlebars = require('express3-handlebars').create({ defaultLayout:'main' })
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+//////////////////////////////////////////////静态资源路径设置ks/////////////
+app.use(express.static(__dirname + '/public'));
+
+//////////////////////////////////////////////路由部分ks/////////////////////
 app.get('/', function(req, res){
 	res.render('home');
 });
 app.get('/about', function(req, res){
-	res.render('about');
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: randomFortune });
 });
 
 
+///////////////////////////////////////////////中间件部分ks////////////////////
 // 定制404 页面
 app.use(function(req, res){
-	res.type('text/plain');
+	/*res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.send('404 - Not Found');*/
+	res.status(404);
+	res.render('404');
 });
 // 定制500 页面
 app.use(function(err, req, res, next){
-	console.error(err.stack);
+	/*console.error(err.stack);
 	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.send('500 - Server Error');*/
+	res.status(500);
+	res.render('500');
 });
 /*app.listen(app.get('port'), function(){
 	console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
 });*/
+
+var fortunes = [
+"Conquer your fears or they will conquer you.",
+"Rivers need springs.",
+"Do not fear what you don't know.",
+"You will have a pleasant surprise.",
+"Whenever possible, keep it simple.",
+];
 
 module.exports = app;
